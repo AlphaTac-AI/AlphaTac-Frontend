3a5e588062483f98bee45d1991c4e312c617e8c2
Jiamin Ning <jiamin@poprx.ca>
2018-07-10 21:45:33 -0400

    use_react_loadable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
