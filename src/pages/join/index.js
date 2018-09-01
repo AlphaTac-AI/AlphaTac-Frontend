35169eb4e5fe3162be2500fd25501de241f3caac
Vector-S <jiraiya812@gmail.com>
2018-09-01 02:15:44 -0400

    added join us content

--- src/pages/join/index.js
+++ src/pages/join/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Paragraph from "../../common/components/paragraph";
 
 import './index.less';
 
@@ -6,7 +7,9 @@ export default class Home extends Component {
   render() {
     return (
       <div className="join-page">
-        <h3>Join Us</h3>
+        <Paragraph title="加入我们" contents={["join.applicationGuide"]}/>   
+        <Paragraph title="关于面试" contents={["join.aboutInterview"]}/>  
+
       </div>
     );
   }
